acdd7ef682e6e8f7fd1073ac7f4e38035191919d
ShinCoder <88935125+ShinCoder@users.noreply.github.com>
2022-11-12 10:36:11 +0700

    GET + POST

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const Joi = require('joi');
+const express = require('express');
+const app = express();
 
-app.use(morgan('combined'))
-app.get('/home', (req, res) => {
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "courses1" },
+  { id: 2, name: "courses2" },
+  { id: 3, name: "courses3" }
+];
+
+// Handling GET request
+app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
+app.get('/api/courses', function (req, res) {
+  res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(function (course) {
+    return course.id === parseInt(req.params.id);
+  });
+
+  if (!course) res.status(404).send("The course with the given id was not found");
+  res.send(course);
+});
+// Handling GET request end
+
+// Handling POST request
+app.post('/api/courses', (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+
+  const result = schema.validate(req.body);
+  
+  if(result.error){
+    res.status(400).send(result.error.details[0].message);
+    return;
+  } 
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
+  courses.push(course);
+  res.send(course);
+});
+// Handling POST request end
+
+// Handling PUT request
+
+// Handling PUT request end
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
